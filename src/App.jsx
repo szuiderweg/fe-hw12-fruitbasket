1104007d6eb18b13099f419a52a5968cbe23e9c8
szuiderweg <svmzuiderweg@hotmail.com>
2024-04-07 22:20:37 +0200

    logo toegevoegd, remote Github repo aangemaakt

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 
 import './App.css'
-
+import fruitlogo from "./assets/screenshot-logo.png";
 function App() {
 
   return (
     <>
+        <img src={fruitlogo} alt="fruitlogo"/>
         <h1>Fruitmand bezorgservice</h1>
     </>
   )
